7fc9d8624e51a2914d0f7db32da58bd4f97d3fbd
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-09-11 14:15:32 +0700

    fix(admin-ui): wrap components with Suspense
    wrap component that uses `useSearchParams` with `<Suspense />` to make sure the page does not de-opt
    to client-side-rendering

--- src/app/admin/product/list/page.tsx
+++ src/app/admin/product/list/page.tsx
@@ -1,8 +1,14 @@
 import { ProductDataTable } from '@/modules/product/product-data-table'
 
 import { random, sleep } from 'radash'
+import { Suspense } from 'react'
 
 export default async function ProductPage() {
   await sleep(random(1000, 2000))
-  return <ProductDataTable />
+
+  return (
+    <Suspense>
+      <ProductDataTable />
+    </Suspense>
+  )
 }
--- src/app/admin/user/list/page.tsx
+++ src/app/admin/user/list/page.tsx
@@ -1,5 +1,14 @@
 import { UserDataTable } from '@/modules/user'
 
-export default function ListUserPage() {
-  return <UserDataTable />
+import { random, sleep } from 'radash'
+import { Suspense } from 'react'
+
+export default async function ListUserPage() {
+  await sleep(random(800, 1000))
+
+  return (
+    <Suspense>
+      <UserDataTable />
+    </Suspense>
+  )
 }
