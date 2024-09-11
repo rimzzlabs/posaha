179328e99225c1c8c39d92a8412e4840674af4ec
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-09-11 22:59:10 +0700

    style(sidebar, layout): adjust styling sidebar and wrapper

--- src/layouts/wrappers/private-wrapper.tsx
+++ src/layouts/wrappers/private-wrapper.tsx
@@ -6,10 +6,10 @@ import { ComponentPropsWithoutRef } from 'react'
 
 export function PrivateWrapper(props: ComponentPropsWithoutRef<'main'>) {
   return (
-    <main className={cn('pt-16 pl-72', props.className)} {...props}>
-      <div className='p-4 md:p-10 bg-muted'>
-        <div className='bg-background p-6 md:p-10 rounded-xl'>
-          <ScrollArea className='h-[calc(100vh-4rem-2rem-3rem)] md:h-[calc(100vh-4rem-5rem-5rem)] pr-4'>
+    <main className={cn('pt-16 pl-72 max-lg:pl-0', props.className)} {...props}>
+      <div className='p-4 lg:p-10 bg-muted'>
+        <div className='bg-background p-6 lg:p-10 rounded-xl'>
+          <ScrollArea className='h-[calc(100vh-4rem-2rem-3rem)] lg:h-[calc(100vh-4rem-5rem-5rem)] pr-4'>
             {props.children}
           </ScrollArea>
         </div>
