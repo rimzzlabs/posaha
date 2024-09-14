02a26ec454eb95e8011154b3950b7ec01474a1d1
Rizki Maulana Citra (rimzzlabs) <62492410+rimzzlabs@users.noreply.github.com>
2024-09-14 20:15:34 +0700

    fix(usesignin, headercontainer): invalid redirect URL (#9)
    this leads to not found page, because page `/admin` was actually changed
    to `/app`, we simply change the URL string to a new one

--- src/layouts/header/header-container.tsx
+++ src/layouts/header/header-container.tsx
@@ -7,7 +7,7 @@ export function HeaderContainer() {
   return (
     <header className='fixed bg-background top-0 inset-x-0 border-b z-40'>
       <div className='h-16 flex items-center px-10'>
-        <Link href='/admin' className='inline-flex items-center gap-x-2'>
+        <Link href='/app' className='inline-flex items-center gap-x-2'>
           <LandPlotIcon className='stroke-primary' size='1.25em' />
           <p className='text-lg font-bold'>Posaha</p>
         </Link>
