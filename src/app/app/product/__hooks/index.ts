5d5d1fb3502e7c910106e707ecb738c168cb5ffb
Rizki Maulana Citra <rmaulana.citra@gmail.com>
2024-09-22 22:43:04 +0700

    fix: type error for missing module

--- src/app/app/product/__hooks/index.ts
+++ src/app/app/product/__hooks/index.ts
@@ -1,4 +1,3 @@
 export * from './use-create-product'
 export * from './use-create-category'
-export * from './use-product-list'
 export * from './use-update-product'
--- src/app/app/product/__hooks/use-product-list.ts
+++
@@ -1,7 +0,0 @@
-import { sortedProductListAtom } from '@/states/product'
-
-import { useAtomValue } from 'jotai'
-
-export function useProductList() {
-  return useAtomValue(sortedProductListAtom)
-}
